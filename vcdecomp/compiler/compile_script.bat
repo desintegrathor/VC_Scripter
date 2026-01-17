d50eb21b64d7b766ebef2f979a6a5a9e47b81f3b
desintegrathor <166166711+desintegrathor@users.noreply.github.com>
2026-01-17 16:36:20 +0100

    fix: use full path to scmp.exe in bat file
    Added cd /d %~dp0 to change to bat file directory
    Use %~dp0scmp.exe to call compiler with full path

--- vcdecomp/compiler/compile_script.bat
+++ vcdecomp/compiler/compile_script.bat
@@ -2,4 +2,8 @@
 REM Compile script for validation
 REM Usage: compile_script.bat source.c output.scr output.h
 
-scmp "%1" "%2" "%3"
+REM Change to the directory where this bat file is located
+cd /d "%~dp0"
+
+REM Call scmp from the current directory
+"%~dp0scmp.exe" "%1" "%2" "%3"
