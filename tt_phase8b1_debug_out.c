6a3704d13806043551740e3c748472f08a777cf8
desintegrathor <166166711+desintegrathor@users.noreply.github.com>
2026-01-19 18:37:10 +0100

    cleanup
    dsa

--- tt_phase8b1_debug_out.c
+++
@@ -1,6 +0,0 @@
-Traceback (most recent call last):
-  File "C:\Users\flori\source\repos\VC_Scripter\run_with_debug.py", line 10, in <module>
-    exec(f.read())
-    ~~~~^^^^^^^^^^
-  File "<string>", line 18, in <module>
-ImportError: attempted relative import with no known parent package
